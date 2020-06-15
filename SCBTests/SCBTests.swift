1b87560365edaa64b2b8de14279c908b4e1fb52d
Sandeep <sandeepkumart@maveric-systems.com>
2020-06-15 10:13:21 +0530

    Initial Commit

---
+++ SCBTests/SCBTests.swift
@@ -0,0 +1,34 @@
+//
+//  SCBTests.swift
+//  SCBTests
+//
+//  Created by MAPRO08 on 15/06/20.
+//  Copyright © 2020 MAPRO08. All rights reserved.
+//
+
+import XCTest
+@testable import SCB
+
+class SCBTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
